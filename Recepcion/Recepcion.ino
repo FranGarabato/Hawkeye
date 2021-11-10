80276e05492a11951c28a28eff768ffcbe667c1f
FranGarabato <garabato1069@gmail.com>
2021-11-10 10:46:00 -0300

    Feature: Declarar Librerias, Pines y Variables

--- Recepcion/Recepcion.ino
+++ Recepcion/Recepcion.ino
@@ -1,3 +1,14 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+ 
+//Declaremos los pines CE y el CSN
+#define CE_PIN 4
+#define CSN_PIN 5
+ 
+//Variable con la dirección del canal que se va a leer
+byte direccion[5] ={'c','a','n','a','l'};
+
 void setup() {
   // put your setup code here, to run once:
 
