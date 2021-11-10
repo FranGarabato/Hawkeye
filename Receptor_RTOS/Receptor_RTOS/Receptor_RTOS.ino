a611ca48244c61d50f12c56e7b0324c3ff96a5f1
FranGarabato <garabato1069@gmail.com>
2021-11-10 12:35:13 -0300

    Feature: Definir pines y variables

--- Receptor_RTOS/Receptor_RTOS/Receptor_RTOS.ino
+++ Receptor_RTOS/Receptor_RTOS/Receptor_RTOS.ino
@@ -1,3 +1,27 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+//Declaremos los pines CE y el CSN
+#define CE_PIN 4
+#define CSN_PIN 5
+ 
+//Variable con la dirección del canal que se va a leer
+byte direccion[5] ={'c','a','n','a','l'}; 
+
+//creamos el objeto radio (NRF24L01)
+RF24 radio(CE_PIN, CSN_PIN);
+
+//vector para los datos recibidos
+float datos[3];
+
+TaskHandle_t Task1;
+TaskHandle_t Task2;
+
 void setup() {
   // put your setup code here, to run once:
 
