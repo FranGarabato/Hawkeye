63b76a9bdd5613766d318439a177f4e27f48084c
FranGarabato <garabato1069@gmail.com>
2021-11-10 09:39:02 -0300

    Feature: Declarar Librerias

--- Transmisor_RTOS/Transmisor_RTOS.ino
+++ Transmisor_RTOS/Transmisor_RTOS.ino
@@ -1,4 +1,11 @@
 #include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+//Declaremos los pines CE y el CSN
+#define CE_PIN 4
+#define CSN_PIN 5
 
 void setup() {
 
