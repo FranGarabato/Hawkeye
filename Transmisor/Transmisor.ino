8c8378621a897528b69abb889abe8a22204b49fc
FranGarabato <garabato1069@gmail.com>
2021-11-10 10:58:53 -0300

    Feature: Configurar Inicializacion de Servicios

--- Transmisor/Transmisor.ino
+++ Transmisor/Transmisor.ino
@@ -31,7 +31,12 @@ TinyGPSPlus gps;
 SoftwareSerial ss(RXPin, TXPin);
 
 void setup() {
-  // put your setup code here, to run once:
+  //inicializamos el NRF24L01 
+  radio.begin();
+  Serial.begin(9600); 
+ //Abrimos un canal de escritura
+ radio.openWritingPipe(direccion);
+ ss.begin(GPSBaud);
 
 }
 
