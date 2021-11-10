bbaf88662c2c751c366dce8ddb3d8f584fe5cb78
FranGarabato <garabato1069@gmail.com>
2021-11-10 11:28:50 -0300

    Feature: Declarar Pines y Variables

--- Receptor/Receptor.ino
+++ Receptor/Receptor.ino
@@ -6,6 +6,19 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+//Declaremos los pines CE y el CSN
+#define CE_PIN 4
+#define CSN_PIN 5
+ 
+//Variable con la dirección del canal que se va a leer
+byte direccion[5] ={'c','a','n','a','l'}; 
+
+//creamos el objeto radio (NRF24L01)
+RF24 radio(CE_PIN, CSN_PIN);
+
+//vector para los datos recibidos
+float datos[3]
+
 void setup() {
   // put your setup code here, to run once:
 
