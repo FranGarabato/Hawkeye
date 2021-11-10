b0205fe8b539f938061c4e164d4e4a615a278d0a
FranGarabato <garabato1069@gmail.com>
2021-11-10 10:36:12 -0300

    Feature: Configuracion Tranceptor

--- Tranceptor/Tranceptor.ino
+++ Tranceptor/Tranceptor.ino
@@ -5,6 +5,11 @@
 #define CE_PIN 4
 #define CSN_PIN 5
 
+byte direccion[5] ={'c','a','n','a','l'};
+
+//creamos el objeto radio (NRF24L01)
+RF24 radio(CE_PIN, CSN_PIN);
+
 void setup() {
   // put your setup code here, to run once:
 
