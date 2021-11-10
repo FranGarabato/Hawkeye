63691586a31046d13c06a500db34a7010b40e901
FranGarabato <garabato1069@gmail.com>
2021-11-10 10:20:17 -0300

    Feature: Declarar Variables

--- GPS/GPS.ino
+++ GPS/GPS.ino
@@ -1,6 +1,10 @@
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 
+static const int RXPin = 16, TXPin = 17;
+static const uint32_t GPSBaud = 9600;
+
+
 void setup() {
   // put your setup code here, to run once:
 
